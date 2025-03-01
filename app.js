88e1d3e9ebbd51ae55ec4b25b32193d4bfb4cad4
helyousfi <visioncomputer200@gmail.com>
2025-03-01 23:54:31 +0100

    changed app.js

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ import errorMiddleware from './middlewares/error.middleware.js';
 import cookieParser from 'cookie-parser';
 import arcjetMiddleware from './middlewares/arcjet.middleware.js';
 import worflowRouter from './routes/workflow.routes.js';
+
 // Create an express app
 const app = express();
 
@@ -16,11 +17,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // process form data
 app.use(errorMiddleware);
+app.use(arcjetMiddleware);
 app.use(cookieParser());
 
-
 // Routes
-app.use(arcjetMiddleware);
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
--- config/arcjet.js
+++ config/arcjet.js
@@ -1,5 +1,4 @@
 import arcjet, { shield, detectBot, tokenBucket } from "@arcjet/node";
-import express from "express";
 import { ARCJET_KEY } from "./env.js";
 
 const aj = arcjet({
